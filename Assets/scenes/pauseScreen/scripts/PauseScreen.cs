a3871357d4c012ffe03e2f5314b84c4960174536
jkaak <jason.kaak@gmail.com>
2014-04-04 12:40:14 -0400

    pause option menu

---
+++ Assets/scenes/pauseScreen/scripts/PauseScreen.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScreen : MonoBehaviour {
+
+	public Texture backgroundTexture;
+
+	void OnGUI(){
+		//Display background
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+
+		//Display buttons
+		if(GUI.Button(buttonRect(1), "Options")) {
+		}
+		
+		if(GUI.Button(buttonRect(2), "High Scores")) {
+
+		}
+		
+		if(GUI.Button(buttonRect(3), "Help")) {
+		
+		}
+		
+		if(GUI.Button(buttonRect(4), "Exit")) {
+		
+		}
+		
+
+	}
+	
+	Rect buttonRect(int buttonNumber){
+		return new Rect(Screen.width * 0.25f, top(buttonNumber), Screen.width * 0.5f, Screen.height * 0.1f);
+	}
+	
+	float top(int buttonNumber){
+		float buttonPlacementY = 0.2f;
+		float buttonOffsetY = 0.125f;
+		
+		return Screen.height * (buttonPlacementY + buttonOffsetY * buttonNumber);
+	}
+	
+}
