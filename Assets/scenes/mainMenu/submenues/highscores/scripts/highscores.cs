a2a3e88270bddf048eed887edc70ecc524bcefda
Mark McKenna <mmckenna91@gmail.com>
2014-04-04 11:28:02 -0400

    hooked up highscore dummy screen to main menu

--- Assets/scenes/mainMenu/submenues/highscores/scripts/highscores.cs
+++ Assets/scenes/mainMenu/submenues/highscores/scripts/highscores.cs
@@ -1,48 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour {
+public class highscores : MonoBehaviour {
 
 	public Texture backgroundTexture;
-
+	
 	void OnGUI(){
 		//display background texture
 		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), backgroundTexture);
-
+		
 		//displays our buttons
-		if(GUI.Button(buttonRect(1), "Play Game")) {
-			Application.LoadLevel(0);
-		}
-
-		if(GUI.Button(buttonRect(2), "Options")) {
-			Application.LoadLevel(1);
-		}
-		
-		if(GUI.Button(buttonRect(3), "Highscores")) {
-			Application.LoadLevel(3);
-		}
-		
-		if(GUI.Button(buttonRect(4), "Help")) {
-			print("Clicked help");
-		}
-		
-		if(GUI.Button(buttonRect(5), "Exit")) {
-			Application.Quit();
-		}
-
-		if(GUI.Button(buttonRect(6), "Facebook")) {
-			print("facebook");
+		if(GUI.Button(buttonRect(1), "Back to main menu")) {
+			Application.LoadLevel(2);
 		}
 	}
-
+	
 	Rect buttonRect(int buttonNumber){
 		return new Rect(Screen.width * 0.25f, top(buttonNumber), Screen.width * 0.5f, Screen.height * 0.1f);
 	}
-
+	
 	float top(int buttonNumber){
 		float buttonPlacementY = 0.2f;
 		float buttonOffsetY = 0.125f;
-
+		
 		return Screen.height * (buttonPlacementY + buttonOffsetY * buttonNumber);
 	}
 }
