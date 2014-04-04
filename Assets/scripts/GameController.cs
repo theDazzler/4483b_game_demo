3ad1d1c2511dae849d1cc3b67fe0e182871116ee
wroy <wroy@uwo.ca>
2014-04-04 14:19:29 -0400

    updating gamecontroller and prefabs

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -5,12 +5,23 @@ public class GameController : MonoBehaviour
 {
 	public Vector2 spawnRangeX;
 	public Vector2 spawnRangeY;
-	public GameObject food;
 	public int foodCount;
 	public float spawnDelay;
 	public float levelWait;
-
-
+	
+	//good foods 
+	public GameObject apple;
+	public GameObject brussel;
+	public GameObject roastChicken;
+	public GameObject water;
+	public GameObject cottonCandy;
+	
+	//bad foods
+	public GameObject donut;
+	public GameObject friedChicken;
+	public GameObject cake;
+	public GameObject chips;
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -23,19 +34,19 @@ public class GameController : MonoBehaviour
 		if (Input.GetKeyDown ("space") || Input.GetKeyDown ("p")) {
 			Application.LoadLevel(6);
 		}
-
 	}
-
+	
 	//spawn food using timer
 	IEnumerator spawnFood()
 	{
 		yield return new WaitForSeconds(levelWait);
-
-		for (int i = 0; i < foodCount; i++)
+		
+		//for (int i = 0; i < foodCount; i++)
+		while (true)
 		{
 			Vector2 spawnPosition = new Vector2 (Random.Range (spawnRangeX.x, spawnRangeX.y), Random.Range (spawnRangeY.x, spawnRangeY.y));
 			Quaternion spawnRotation = Quaternion.identity;
-			Instantiate(food, spawnPosition, spawnRotation);
+			Instantiate(apple, spawnPosition, spawnRotation);
 			yield return new WaitForSeconds(spawnDelay);
 		}
 	}
